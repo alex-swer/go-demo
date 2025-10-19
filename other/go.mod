cff6322f7400def0c43d46fefea134d6e32f0f10
Alexander Parphinovich <alex.swer.x@gmail.com>
2025-10-19 18:38:44 +0300

    Refined documentation on number conversion, enhancing clarity with additional examples and a comprehensive powers of two table.

---
+++ other/go.mod
@@ -0,0 +1,5 @@
+module go-demo
+
+go 1.24.1
+
+require golang.org/x/sys v0.37.0 // indirect
---
+++ other/hello_win.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"golang.org/x/sys/windows"
+	"syscall"
+	"unsafe"
+)
+
+func main() {
+	user32 := windows.NewLazySystemDLL("user32.dll")
+	messageBoxW := user32.NewProc("MessageBoxW")
+	title := "Заголовок"
+	text := "Hello from Go!"
+	titlePtr, _ := syscall.UTF16PtrFromString(title)
+	textPtr, _ := syscall.UTF16PtrFromString(text)
+	messageBoxW.Call(0,
+		uintptr(unsafe.Pointer(textPtr)),
+		uintptr(unsafe.Pointer(titlePtr)),
+		0)
+}
